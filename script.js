23f36eebf3c21466dcf1ef77056abf2bbd5ed301
Pål Hamel <pal.hamel@gmail.com>
2020-07-15 20:38:23 +0200

    Div update in code

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 if (!("requestMIDIAccess" in navigator)) {
+  console.log('☹️ WebMIDI is not supported in this browser.')
   document.body.innerHTML = `<h1>:-/</h1><p>I'm sorry, but your browser does not support the WebMIDI API ☹️🚫🎹</p>`;
+} else {
+  console.log('🙌🏻 YES, happy days! This browser supports WebMIDI!');
 }
 
 navigator.requestMIDIAccess()
   .then((access) => {
+    console.log('midi access:', access);
     const inputs = access.inputs;
     const outputs = access.outputs;
     const inputText = [];
     const outputText = [];
     
     access.onstatechange = function(e) {
-      // Print information about the (dis)connected MIDI controller
-      console.log(e.port.name, e.port.manufacturer, e.port.state);
+      // MIDI controller:
+      console.log('Hardware:', e.port.name, e.port.manufacturer, '<<STATE>>',e.port.state);
+      // console.log('check e.port:', e.port)
     };
-
+    // INPUTS:
     inputs.forEach((midiInput) => { 
-      inputText.push(`FOUND: ${midiInput.name}`);
+      inputText.push(` FOUND: ${midiInput.name} // `);
         midiInput.onmidimessage = function(message) {                  
           document.querySelector("#messages").innerText +=  `# ${midiInput.name}
-        ${new Date().getHours()}
+        ${new Date()}
         ==================
         - Status: ${message.data[0]}
         - Data 1: ${message.data[1]}
@@ -26,12 +31,14 @@ navigator.requestMIDIAccess()
         ==================\n\n`;
         }
     })
-  
+    // OUTPUTS:
     outputs.forEach((midiOutput) => { 
-      outputText.push(`FOUND: ${midiOutput.name}`);
+      outputText.push(` FOUND: ${midiOutput.name} is: ${midiOutput.state} //`);
+      // console.log('midiOutput', midiOutput)
     })
       
     document.querySelector("#inputs").innerText = inputText.join('');
     document.querySelector("#outputs").innerText = outputText.join('');  
  
   });
+
